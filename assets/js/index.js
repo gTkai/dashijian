84c3eb9a277ac008bb9cd8e2fafce68d0e696cd2
zyh <2638857570@qq.com>
2021-10-26 14:08:19 +0800

    首页功能开发完成

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+$(function () {
+    var layer = layui.layer;
+    getUserInfo()
+    function getUserInfo() {
+        $.ajax({
+            type: 'get',
+            url: '/my/userinfo',
+            headers: {
+                Authorization: localStorage.getItem('token')
+            },
+            success: function (res) {
+                if (res.code != 0) {
+                    return layer.msg('获取用户信息错误')
+                }
+                renderAvatar(res.data)
+            },
+        })
+    }
+    function renderAvatar(user) {
+        var name = user.nickname || user.username
+        $('#welcome').html('欢迎' + name)
+        if (user.user_pic) {
+            $('.layui-nav-img').attr('src',user.user_pic)
+            $('.text-avatar').hide()
+        }else{
+            $('.layui-nav-img').hide()
+            $('.text-avatar').show()
+            $('.text-avatar').text(name[0].toUpperCase())
+        }
+    }
+})
