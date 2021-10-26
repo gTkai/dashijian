8d5362a885bb1394579ba95a975833e2b1657660
zyh <2638857570@qq.com>
2021-10-26 16:06:21 +0800

    首页安全

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,17 @@
-$.ajaxPrefilter(function(options){
+$.ajaxPrefilter(function (options) {
     console.log(options);
-    options.url='http://www.liulongbin.top:3008'+options.url
+    options.url = 'http://www.liulongbin.top:3008' + options.url;
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function (res) {
+        console.log(res);
+        if (res.responseJSON.code == 1 && res.responseJSON.message == '身份认证失败！') {
+            console.log(111);
+            localStorage.removeItem('token')
+            location.replace('login.html')
+        }
+    }
 })
